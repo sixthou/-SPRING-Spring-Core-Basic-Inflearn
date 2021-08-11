fc6bc96ab45a1af28a6e2c22240ffbe6664c5a31
sixthou <justyuk@naver.com>
2021-08-11 23:51:06 +0900

    feat : Order 생성

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,6 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import javax.naming.spi.DirObjectFactory;
 
 public class FixDiscountPolicy implements DiscountPolicy {
 

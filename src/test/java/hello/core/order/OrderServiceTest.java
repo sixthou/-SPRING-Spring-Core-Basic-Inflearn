01cf3ee38affbd8593712b57f1b2736e23203632
sixthou <justyuk@naver.com>
2022-01-20 22:22:42 +0900

    feat : 정률 할인 정책 구현 및 테스트 작성

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -20,6 +20,7 @@ public class OrderServiceTest {
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
+
         //when
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
